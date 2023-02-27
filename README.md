6e3ab2174c5529781012a9a6551b8335f4e6ece3
Ali Hadi Shenasa <38480296+AliShenasa@users.noreply.github.com>
2023-02-27 15:56:26 -0800

    Updated script to use spatial period

--- README.md
+++ README.md
@@ -16,22 +16,22 @@ Setup anaconda environment:
 ### VRSpriteMaker.py setup
 Input parameters using flags:
 
---stripecount OR -n to specify the number of stripes. Default: 5
+--spatialperiod OR --sp or -p to specify the spatialperiod. Default: 60
 
 --resolution OR -r to specify the resolution. Default: 400, 300
-
---stripewidth OR -w to specify the width of the stripe in pixels. Default: 10
 
 --stripecolor OR -s to specify the color of the stripe in rgb. Default: 0,0,255 aka blue
 
 --backgroundcolor OR -b to specify the color of the background in rgb. Default: 50,50,50 aka gray
 
+--outfile OR -o to specify the filename to save the image as. Default: sprite.png
+Include the filename extension so the script knows what format to save the image in.
 
 Program will output a png in the directory it was run in.
 
 Example usage:
 
-```python VRSpriteMaker.py -n 7 -r 400,300 -s 0,0,255 -b 50,50,50```
+```python VRSpriteMaker.py --sp 60 -r 400,300 -s 0,0,255 -b 50,50,50 -o sprite_60deg.png```
 
 ### VRSpriteMaker.ipynb setup
 Open jupyter notebook:
--- VRSpriteMaker.py
+++ VRSpriteMaker.py
@@ -1,42 +1,80 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from PIL import Image, ImageDraw
 import argparse
+from PIL import Image, ImageDraw
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--stripecount','-n', type=int, action='store', help='number of stripes')
-parser.add_argument('--resolution','-r', type=str, action='store', help='resolution of sprite')
-parser.add_argument('--stripewidth','-w', type=int, action='store', help='width of stripe')
-parser.add_argument('--stripecolor','-s', type=str, action='store', help='rgb of the strip')
-parser.add_argument('--backgroundcolor','-b', type=str, action='store', help='rgb of the background')
+def create_parser():
+    """Creates the parser object 
 
-args = parser.parse_args()
+    Returns:
+        args: object storing all of the arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--spatialperiod', '--sp', '-p',
+                        type=int, action='store',
+                        help='number of stripes')
+    parser.add_argument('--resolution','-r',
+                        type=str, action='store',
+                        help='resolution of sprite')
+    parser.add_argument('--stripecolor','-s',
+                        default="0,0,255",
+                        type=str, action='store',
+                        help='rgb of the strip')
+    parser.add_argument('--bgcolor','-b',
+                        default="50,50,50",
+                        type=str, action='store',
+                        help='rgb of the background')
+    parser.add_argument('--outfile','-o',
+                        type=str, action='store',
+                        help='file to save image to')
 
-stripecount = (args.stripecount if args.stripecount != None else 5)
-resolution = (args.resolution.split(',') if args.resolution != None else ["400","300"])
-width = int(resolution[0])
-height = int(resolution[1])
-
-if(args.stripecolor != None):
-    stripecolor = tuple([int(item) for item in args.stripecolor.split(',')])
-else:
-    stripecolor = (0,0,255) #blue
-
-if(args.backgroundcolor != None):
-    backgroundcolor = tuple([int(item) for item in args.backgroundcolor.split(',')])
-else:
-    backgroundcolor = (50,50,50) #gray
-
-stripewidth = (args.stripewidth if args.stripewidth != None else 10)
+    return parser.parse_args()
 
 
-img = Image.new('RGB', (width, height), color=backgroundcolor)
-draw = ImageDraw.Draw(img)
+def create_image(spatial_period=60, 
+                 width=400, height=300,  
+                 stripe_color=(0,0,255), 
+                 bg_color=(50,50,50)):
+    """Creates an image with alternating stripes
 
-for i in range(0, stripecount):
-    draw.rectangle([(i*width/stripecount, 0),
-                    (i*width/stripecount+stripewidth, height)],
-                    fill=stripecolor)
+    Args:
+        spatial_period (int, optional): Amount of 360 view a light and dark stripe occupy. Defaults to 60. 
+        width (int, optional): Width of image. Defaults to 400.
+        height (int, optional): Height of image. Defaults to 300.
+        stripe_color (tuple, optional): Color of light stripe. Defaults to (0,0,255).
+        bg_color (tuple, optional): Color of dark stripe. Defaults to (50,50,50).
 
-img.save('sprite.png')
+    Returns:
+        ~PIL.Image.Image: image
+    """
+
+    img = Image.new('RGB', (width, height), color=bg_color)
+    draw = ImageDraw.Draw(img)
+
+    stripe_count = int(360/spatial_period)
+    stripe_width = int(spatial_period/2 * (width/360))
+
+    for i in range(0, stripe_count):
+        draw.rectangle([(i*width/stripe_count, 0),
+                        (i*width/stripe_count+stripe_width, height)],
+                        fill=stripe_color)
+    return img
+
+if __name__ == "__main__":
+
+    # Create parser
+    args = create_parser()
+
+    # Read in arguments
+    spatial_period = args.spatialperiod
+    resolution = args.resolution.split(',')
+    width = int(resolution[0])
+    height = int(resolution[1])
+    stripe_color = tuple([int(item) for item in args.stripecolor.split(',')])
+    bg_color = tuple([int(item) for item in args.bgcolor.split(',')])
+    outfile = args.outfile if args.outfile is not None else "sprite.png"
+
+    # Create image
+    img = create_image(spatial_period, width, height, stripe_color, bg_color)
+    img.save(outfile)
